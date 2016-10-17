8fbeff0653c7a6a878105f2fa907f2b5f3622096
gnzlopez <gnz.lopez@hotmail.com>
2016-10-16 22:58:39 -0300

    Agregue clases Libro y Editorial

---
+++ OMB/Entidades/Editorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Editorial
+    {
+
+        public Guid IDEditorial { get; set; }
+        public string Nombre { get; set; }
+        public string Domicilio { get; set; }
+        public string AmpliacionDomicilio { get; set; }
+        public string CodigoPostal { get; set; }
+
+        public virtual Localidad Localidad { get; set; }
+
+    }
+}
---
+++ OMB/Entidades/Libro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Libro
+    {
+        public Guid IDLibro { get; set; }
+        public string Isbn { get; set; }
+        public string Isbn10 { get; set; }
+        public string Titulo { get; set; }
+        public string Subtitulo { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public int Paginas { get; set; }
+        public string DatosEnvio { get; set; }
+        public Image Portada { get; set; }
+        
+        public virtual Editorial Editorial { get; set; }
+        }
+    }
+}
